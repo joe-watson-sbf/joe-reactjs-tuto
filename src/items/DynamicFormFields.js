7fed08d3381352380e3ff12d8ca5bf542cd8f887
Joe Watson SBF <joewatson40@hotmail.com>
2023-01-18 18:59:17 -0500

    first commit

--- src/items/DynamicFormFields.js
+++ src/items/DynamicFormFields.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const initialProduct = {
-    name: '',
+const initialState = {
+    name:'',
     price: 0,
-    quantity: 0
+    quantity: 0,
 }
 
 
@@ -11,75 +11,88 @@ const DynamicFormFields = () => {
 
     const [products, setProducts] = React.useState([])
 
-    const addMoreProducts = () => {
-        const list = [...products]
-        list.push(initialProduct)
+    const addMoreProducts=()=>{
+        const list =[...products]
+        list.push(initialState)
         setProducts(list)
     }
 
-    const handleProductChange = (e, index) => {
-        const { name, value } = e.target
-        const product = { ...products[index], [name]: value }
+    const handleProductChange=(e, index)=>{
+        const {name, value} = e.target
+        const product = {...products[index], [name]: value}
         const list = [...products]
         list[index] = product
         setProducts(list)
     }
 
-    const handleRemoveClick = (index) => {
-        const list = [...products]
+    const handleRemoveProduct=(index)=>{
+        const list=[...products]
         list.splice(index, 1)
         setProducts(list)
     }
 
-    const handeSubmit = () => {
+
+    const handleSubmitProducts=()=>{
         alert(JSON.stringify(products, null, 2))
     }
 
 
+
     return (
         <div className='flex-col'>
-            <h1 className='title'>Add & Remove Form Fields dynamically</h1>
-            {products.map((product, index) => {
-                return (
-                    <div className='flex-row input-group' key={index}>
-                        <div>
-                            <label style={{ display: 'block' }}>Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder='Laptop'
-                                value={product.name}
-                                onChange={e => handleProductChange(e, index)}
-                            />
-                        </div>
-                        <div>
-                            <label style={{ display: 'block' }}>Price</label>
-                            <input
-                                name="price"
-                                placeholder='Price'
-                                value={product.price}
-                                onChange={e => handleProductChange(e, index)}
-                            />
-                        </div>
-                        <div>
-                            <label style={{ display: 'block' }}>Quantity</label>
-                            <input
-                                name="quantity"
-                                placeholder='Quantity'
-                                value={product.quantity}
-                                onChange={e => handleProductChange(e, index)}
-                            />
-                        </div>
-                        <span className='delete' onClick={() => handleRemoveClick(index)}> x </span>
-                    </div>
-                )
-            })}
+            <h1 className='title'>
+                Add & Remove Form Fields Dynamically
+            </h1>
 
-            {products.length > 0 ? <div>
-                <button className='add-more' onClick={addMoreProducts}>+ Add More</button>
-                <button className='btn' onClick={handeSubmit}> Submit Products </button>
-            </div>
-                : <button className='btn' onClick={addMoreProducts}>Add Product</button>
+            {
+                products.map((product, index)=> {
+                    return (
+                        <div className='flex-row input-group' key={index}>
+                            <div>
+                                <label>Name</label>
+                                <input 
+                                    type='text'
+                                    name='name'
+                                    value={product.name} 
+                                    onChange={(e)=> handleProductChange(e, index)}
+                                    />
+                            </div>
+
+                            <div>
+                                <label>Price</label>
+                                <input 
+                                    type='text'
+                                    name='price'
+                                    value={product.price} 
+                                    onChange={(e)=> handleProductChange(e, index)}
+                                    />
+                            </div>
+
+                            <div>
+                                <label>Quantity</label>
+                                <input 
+                                    type='text'
+                                    name='quantity'
+                                    value={product.quantity} 
+                                    onChange={(e)=> handleProductChange(e, index)}
+                                    />
+                            </div>
+
+                            <button onClick={()=> handleRemoveProduct(index)} className='delete'> x </button>
+                        </div>
+                    )
+                })
+            }
+
+
+
+
+            {
+                products.length > 0 ? <div>
+                <button onClick={addMoreProducts} className='add-more'>+ Add More</button>
+                <button onClick={handleSubmitProducts} className='btn'>Submit Products</button>
+                </div> : 
+                <button onClick={addMoreProducts} className='btn'>Add Products</button>
             }
 
         </div>
